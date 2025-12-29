2ad67d3a17eff7f394cad94a358905d24c01d257
MCredbear <2363764471@qq.com>
2025-12-30 00:24:46 +0800

    新增: 最近播放 API

---
+++ src/api/record_recent_album.rs
@@ -0,0 +1,49 @@
+use super::request;
+use crate::constant::*;
+use serde_json::{Value, json};
+
+const API: &str = "play-record/album/list";
+
+/// 最近播放专辑
+pub async fn record_recent_album(
+    limit: Option<u32>,
+    cookie: Option<String>,
+    crypto: Option<Crypto>,
+) -> Result<Value, Box<dyn std::error::Error>> {
+    let data = json!({
+        "limit": limit.unwrap_or(100),
+    });
+
+    let resp = request(API.to_string(), data, cookie, crypto.unwrap_or(Crypto::Api)).await?;
+    Ok(resp)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_api_record_recent_album() -> Result<(), Box<dyn std::error::Error>> {
+        let resp = record_recent_album(None, None, Some(Crypto::Api)).await?;
+        println!("{}", resp);
+        Ok(())
+    }
+    #[tokio::test]
+    async fn test_linuxapi_record_recent_album() -> Result<(), Box<dyn std::error::Error>> {
+        let resp = record_recent_album(None, None, Some(Crypto::Linuxapi)).await?;
+        println!("{}", resp);
+        Ok(())
+    }
+    #[tokio::test]
+    async fn test_weapi_record_recent_album() -> Result<(), Box<dyn std::error::Error>> {
+        let resp = record_recent_album(None, None, Some(Crypto::Weapi)).await?;
+        println!("{}", resp);
+        Ok(())
+    }
+    #[tokio::test]
+    async fn test_eapi_record_recent_album() -> Result<(), Box<dyn std::error::Error>> {
+        let resp = record_recent_album(None, None, Some(Crypto::Eapi)).await?;
+        println!("{}", resp);
+        Ok(())
+    }
+}
